95ce588fb92a9de97487e466c9da54903d28aae9
Robert Norrie <robert.norrie2@phs.scot>
2025-11-26 09:26:34 +0000

    run ff by hscp

--- R/Explore_GP_consultations_by_postcode_HSCP.R
+++ R/Explore_GP_consultations_by_postcode_HSCP.R
@@ -40,34 +40,13 @@ names(consultations_hscp) <- consultations_hscp
 GP_hscp_sts_list <- map(consultations_hscp, GP_hscp_sts, data = HSCP_data_to_explore)
 
 # Run Farrington Flexible model -------------------------------------------
-
 ## HSCPs ------------------------------------------------------------------
 
 # Calculate range to plot (from the start of the season)
 GP_hscp_seapoch_list <- map(GP_hscp_sts_list, ~ season_epoch(.x, alarm_year = 2024, alarm_week = 40))
 
-# Set model parameters
-GP_con.noufaily <- list(
-  range = GP_seapoch_list[[1]],  # dynamically calculated
-  noPeriods = 10,
-  b = 1,
-  w = 3,
-  reweight = TRUE,
-  trend = TRUE,
-  populationOffset = TRUE,
-  powertrans = "2/3",
-  fitFun = "algo.farrington.fitGLM.flexible",
-  weightsThreshold = 2.58,
-  pastWeeksNotIncluded = 2,
-  pThresholdTrend = 1,
-  thresholdMethod = "nbPlugin",
-  alpha = 0.05,
-  limit54 = c(5, 4)
-)
-
-
 # Run model (Farrington flexible with noufaily adaptation)
-GP_hb_pc.noufaily <- map(GP_sts_list, farringtonFlexible, GP_con.noufaily)
+GP_hscp_pc.noufaily <- map(GP_hscp_sts_list, farringtonFlexible, GP_con.noufaily)
 
 
 
