73b84056dfb3b357d50acafce37147eaee66fe31
Robert Norrie <robert.norrie2@phs.scot>
2025-11-26 09:23:27 +0000

    Create alarm HB hscp sts list

--- R/Explore_GP_consultations_by_postcode_HSCP.R
+++ R/Explore_GP_consultations_by_postcode_HSCP.R
@@ -24,14 +24,50 @@ Postcode_alarms_to_explore <- alarms_to_explore %>%
   select(consultation_type, Postcode) %>%
   distinct()
 
-###...Explore HSCP alarm data...................................................
+### Explore HSCP alarm data -------------------------------------------------
+### -------------------------------------------------------------------------
 
 # Filter all_hscp_data to only include rows that match
 # consultation_type and hscp to explore this week
 HSCP_data_to_explore <- GP_hscp_data %>%
   semi_join(HSCP_alarms_to_explore, by = c("consultation_type", "HSCP_Name"))
 
+# Vector of consultation names to explore by hscp
+consultations_hscp <- unique(HSCP_data_to_explore$consultation_type) # replace with list of consultation types that we report on
+names(consultations_hscp) <- consultations_hscp
 
+# Create list of sts objects for each consultation type
+GP_hscp_sts_list <- map(consultations_hscp, GP_hscp_sts, data = HSCP_data_to_explore)
+
+# Run Farrington Flexible model -------------------------------------------
+
+## HSCPs ------------------------------------------------------------------
+
+# Calculate range to plot (from the start of the season)
+GP_hscp_seapoch_list <- map(GP_hscp_sts_list, ~ season_epoch(.x, alarm_year = 2024, alarm_week = 40))
+
+# Set model parameters
+GP_con.noufaily <- list(
+  range = GP_seapoch_list[[1]],  # dynamically calculated
+  noPeriods = 10,
+  b = 1,
+  w = 3,
+  reweight = TRUE,
+  trend = TRUE,
+  populationOffset = TRUE,
+  powertrans = "2/3",
+  fitFun = "algo.farrington.fitGLM.flexible",
+  weightsThreshold = 2.58,
+  pastWeeksNotIncluded = 2,
+  pThresholdTrend = 1,
+  thresholdMethod = "nbPlugin",
+  alpha = 0.05,
+  limit54 = c(5, 4)
+)
+
+
+# Run model (Farrington flexible with noufaily adaptation)
+GP_hb_pc.noufaily <- map(GP_sts_list, farringtonFlexible, GP_con.noufaily)
 
 
 
