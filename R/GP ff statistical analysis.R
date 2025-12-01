14992a00b39232bd5f6d99acc919c49553d9aec7
Robert Norrie <robert.norrie2@phs.scot>
2025-12-01 16:14:16 +0000

    Trying to read in all ARI Postcode alarm results (True/False), farrington felx model not liking the data at the moment keeps returning errors. May need to also adapt the model which returns postcode alarms too

--- R/GP ff statistical analysis.R
+++ R/GP ff statistical analysis.R
@@ -41,6 +41,158 @@ ARI_alarms_and_flags <- ari_hb_above_baseline %>%
   inner_join(GP_ARI_Nat_HB_FF_alarms, by = c("week_date", "unit"))
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Produce HSCP and Postcode data
+
+
+
+    ### Explore HSCP alarm data -------------------------------------------------
+    ### -------------------------------------------------------------------------
+
+    # Filter all_hscp_data to only include rows that match
+    # consultation_type and hscp to explore this week
+    HSCP_data_to_explore <- GP_hscp_data
+
+    # Vector of consultation names to explore by hscp
+    consultations_hscp <- unique(HSCP_data_to_explore$consultation_type) # replace with list of consultation types that we report on
+    names(consultations_hscp) <- consultations_hscp
+
+    # Create list of sts objects for each consultation type
+    GP_hscp_sts_list <- map(consultations_hscp, GP_hscp_sts, data = HSCP_data_to_explore)
+
+    # Run Farrington Flexible model -------------------------------------------
+    ## HSCPs ------------------------------------------------------------------
+
+    # Calculate range to plot (from the start of the season)
+    GP_hscp_seapoch_list <- map(GP_hscp_sts_list, ~ season_epoch(.x, alarm_year = 2024, alarm_week = 40))
+
+    # Run model (Farrington flexible with noufaily adaptation)
+    GP_hscp_pc.noufaily <- map(GP_hscp_sts_list, farringtonFlexible, GP_con.noufaily)
+
+    # Tidy HSCP Outputs --------------------------------------------------------
+
+    GP_output_hscp <- map(GP_hscp_pc.noufaily, tidy_outputs)
+
+
+    ### Explore Postcode alarm data (ARI only) ----------------------------------
+    ### -------------------------------------------------------------------------
+
+    # Filter all_hscp_data to only include rows that match
+    # consultation_type and postcode to explore this week (ARI only)
+    Postcode_data_to_explore <- GP_postcode_data %>%
+      filter(consultation_type == "ARI")
+
+        # Vector of consultation names to explore by postcode
+        consultations_postcode <- unique(Postcode_data_to_explore$consultation_type) # replace with list of consultation types that we report on
+        names(consultations_postcode) <- consultations_postcode
+
+        # Create list of sts objects for each consultation type
+        GP_postcode_sts_list <- map(consultations_postcode, GP_postcode_sts, data = Postcode_data_to_explore)
+
+        # Run Farrington Flexible model --------------------------------------------
+        ## Postcodes ---------------------------------------------------------------
+
+        # Calculate range to plot (from the start of the season)
+        GP_postcode_seapoch_list <- map(GP_postcode_sts_list, ~ season_epoch(.x, alarm_year = 2024, alarm_week = 40))
+
+        # Run model (Farrington flexible with noufaily adaptation)
+        GP_postcode_pc.noufaily <- map(GP_postcode_sts_list, farringtonFlexible, GP_con.noufaily)
+
+        # Tidy Postcode Outputs --------------------------------------------------------
+
+        GP_output_postcode <- map(GP_postcode_pc.noufaily, tidy_outputs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Cohen’s Kappa checking how well signaling above/below baseline vs
 # alarm/no alarm agree for the same week
 # This is looking at comparing the same week
